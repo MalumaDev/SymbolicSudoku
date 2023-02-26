cc645b24738dee119bc99151bcc58861f3e27647
grains5 <piano.lu92@gmail.com>
2023-02-26 09:37:03 +0100

    upresult

--- launcher.py
+++ launcher.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-datasets_key = ["mnist4", "mnist9", "emnist4", "emnist9", "fmnist4", "fmnist9", "kmnist4", "kmnist9"]
+datasets_key = ["fmnist4", "fmnist9", "kmnist4", "kmnist9"]
 
 for dataset in datasets_key:
     os.system(f"python main.py --dataset {dataset}")
