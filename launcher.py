42b251834b98ce230ef6ec60507d966169898426
Alerusso199 <84847836+Alerusso199@users.noreply.github.com>
2023-03-19 20:14:37 +0100

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -6,9 +6,10 @@ from tqdm import tqdm, trange
 datasets_key = ["mnist"]#, "emnist", "fmnist", "kmnist", ]
 sizes = [4]#, 9]
 only_dataset = False
+algorithm = 1
 
 for dataset in tqdm(datasets_key):
     for size in sizes:
         d = f"{dataset}{size}"
         for split in trange(11):
-            os.system(f"python main.py --dataset {d} --split {split}" + (" --generate_dataset" if only_dataset else ""))
+            os.system(f"python SymbolicSudoku-main/main.py --dataset {d} --split {split} --algorithm {algorithm}" + (" --generate_dataset" if only_dataset else ""))
